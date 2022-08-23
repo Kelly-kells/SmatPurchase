b8ac8d62aeb5130e753f7cd7207e3af8f4850daf
unknown <kellskelechi@gmail.com>
2022-08-23 16:47:00 +0100

    navStyles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { AppBar, ToolBar, IconButton, Badge, MenuItem, Menu, Typography } from '@mui/material'
 import {ShoppingCart} from "@mui/icons-material"
 import {makeStyles} from "@mui/styles";
-
+import StorefrontIcon from '@mui/icons-material/Storefront';
 const Navbar = () => {
 
     const classes = makeStyles();
@@ -10,8 +10,9 @@ const Navbar = () => {
     <div>
         <AppBar position='fixed' className={classes.appBar} color="inherit">
         <ToolBar>
-            <Typography>
-            <img src="" alt="" height="20px"/>
+            <Typography variant='h6' className={classes.tittle} color="inherit">
+            <StorefrontIcon height="20px" className={classes.image} color="fcf300" />
+            MajorMart
             </Typography>
         </ToolBar>
 
