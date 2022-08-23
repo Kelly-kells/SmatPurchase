e80079bc6687f2664edb6ac78a8fa981ef206324
unknown <kellskelechi@gmail.com>
2022-08-23 12:50:57 +0100

    styles

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import Grid from "@mui/material"
-import Product from './product/Product'
+import React from "react";
+import Grid from "@mui/material";
+import Product from "./product/Product";
 
-const products =[
-    {id:1, name: "watch", description: "check your time", price:"$5"},
-    {id:2, name: "shoes", description: "Run it through", price:"$10"}
- ]
+const products = [
+  { id: 1, name: "watch", description: "check your time", price: "$5" },
+  { id: 2, name: "shoes", description: "Run it through", price: "$10" },
+];
 
 const Products = () => {
   return (
     <main>
-  <Grid container justify="center" spacing={4}>
-    {products.map((product)=>(
-        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product}/>
-        </Grid>
-    ))}
-  </Grid>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
     </main>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
--- src/components/products/product/Product.js
+++ src/components/products/product/Product.js
@@ -1,26 +1,38 @@
-import React from 'react'
-import {Card, CardMedia, Typography, CardContent, CardActions, IconBotton} from "@mui/material"
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  Typography,
+  CardContent,
+  CardActions,
+  IconButton,
+} from "@mui/material";
+import AddShoppingCart from "@mui/icons-material/AddShoppingCart";
+import useStyles from "@mui/styles";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
+  const classes = useStyles();
   return (
     <Card className={classes.root}>
-  <CardMedia  className={ classes.media} image=""  title={product.name}/>
-  <CardContent>
-  <div className={classes.cardComponent}>
-  <Typography varient="h5" gutterBottom>
-    {product.name}
-  </Typography>
-  <Typography varient="h5" >
-    {product.price}
-  </Typography>
-  </div>
-  <Typography variant='h2' color="textSecondary">
-  {product.description}
-  </Typography>
-  </CardContent>
+      <CardMedia className={classes.media} image="" title={product.name} />
+      <CardContent>
+        <div className={classes.cardComponent}>
+          <Typography varient="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography varient="h5">{product.price}</Typography>
+        </div>
+        <Typography variant="h2" color="textSecondary">
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
