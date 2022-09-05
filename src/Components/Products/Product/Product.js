b44c6fda1ffff6614e061c54e9db955b0978897c
Kelly-kells <kellskelechi@gmail.com>
2022-09-05 12:48:12 +0100

    cards

---
+++ src/Components/Products/Product/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Card, CardMedia, CardContent, Typography, CardActions,IconButton}from "@mui/material"
+import {AddShoppingCart} from "@mui/icons-material"
+
+const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
---
+++ src/Components/Products/Products.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Grid from "@mui/material"
+import Product from './Product/Product'
+
+const products = [{
+  id: 1, name:"watch", description: "standard classy watch", price: "50$"
+
+},
+{id:2, name:"macbook", description: "apple macbook", price: "50$"}
+]
+
+const Products = () => {
+
+  return (
+    <main>
+      <Grid container justifyContent="center" spacing={4}>
+ {products.map((product)=>(
+  <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+    <Product/>
+    </Grid>
+ ))}
+      </Grid>
+    </main>
+  )
+}
+
+export default Products
