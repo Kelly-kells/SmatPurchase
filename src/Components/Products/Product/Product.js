97ffd6f125fbe11226e8dc3b97451eae960a81f2
Kelly-kells <kellskelechi@gmail.com>
2022-09-07 13:17:56 +0100

    productStyles

--- src/Components/Products/Product/Product.js
+++ src/Components/Products/Product/Product.js
@@ -1,20 +1,31 @@
 import React from 'react'
-import {Card, CardMedia, CardContent, Typography, CardActions,IconButton}from "@mui/material"
-import {AddShoppingCart} from "@mui/icons-material"
+import { Card, CardMedia, CardContent, Typography, CardActions, IconButton } from "@mui/material"
+import { AddShoppingCart } from "@mui/icons-material"
+import useStyles from "./ProductStyle"
 
-const Product = () => {
-  return (
-    <Card className={classes.root}>
-  <CardMedia className={classes.media} image="" title={product.name}/>
-  <CardContent>
-    <div className={classes.CardContent}>
-      <Typography varient="h5" gutterBottom>
-
-      </Typography>
-    </div>
-  </CardContent>
+const Product = ({ product }) => {
+    const classes= useStyles()
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image="" title={product.name} />
+            <CardContent>
+                <div className={classes.CardContent}>
+                    <Typography varient="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography varient="h5" >
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography varient="h5" color="textSecondary" >
+                    {product.description}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label='Add to cart'> < AddShoppingCart /></IconButton>
+            </CardActions>
         </Card>
-  )
+    )
 }
 
 export default Product
