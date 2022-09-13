ed38561784ade90c9b36d5ba84a2e97710d0d357
Kelly-kells <kellskelechi@gmail.com>
2022-09-13 01:52:38 +0100

    styling

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,30 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { FcShop } from "react-icons/fc"
+import { MdOutlineAddShoppingCart } from "react-icons/md"
+import Badge from 'react-bootstrap/Badge';
+import "./style.css"
 
 
 function CollapsibleExample() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar className='nav' collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand className='logo' ><FcShop /></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-            
+
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
               Dank memes
             </Nav.Link>
+            <Badge pill className='badge'> <MdOutlineAddShoppingCart /><span>2</span></Badge>
           </Nav>
         </Navbar.Collapse>
       </Container>
